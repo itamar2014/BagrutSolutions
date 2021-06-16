9e1daf2bde24e5a42ff923b69c6cb913e5407fb1
LightOut <itamarpalmon@gmail.com>
2021-06-16 21:44:36 +0300

    solution

--- src/Bagrut2018/Chapter2/fourthQuestion.java
+++ src/Bagrut2018/Chapter2/fourthQuestion.java
@@ -14,21 +14,6 @@ public class fourthQuestion {
 
         return last;
     }
-    public static void main(String[] args){
 
-        fourthQuestion f = new fourthQuestion();
-
-        Stack<Integer> stk = new Stack<>();
-        stk.add(1);
-        stk.add(6);
-        stk.add(32);
-        stk.add(5);
-        stk.add(7);
-        stk.add(4);
-        stk.add(9);
-
-        System.out.println(f.lastAndRemove(stk) + "\n");
-
-    }
 
 }
